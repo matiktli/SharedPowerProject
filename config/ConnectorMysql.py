f64dae0aa3e5b2effd4dfa6986fc89647c74349a
matikitli <m.kitlinos@gmail.com>
2017-12-20 02:13:25 +0000

    Branch checkout

--- config/ConnectorMysql.py
+++ config/ConnectorMysql.py
@@ -5,16 +5,12 @@ import pymysql
 # Open database connection
 # (domain,username,password,database_name)
 db = pymysql.connect("localhost","adminSharedPower","password","SharedPower_db" )
-
 # Create cursor object
 cursor = db.cursor()
-
 # Execute SQL query using execute() method.
 cursor.execute("SELECT VERSION()")
-
 # Fetch a single row using fetchone() method.
 data = cursor.fetchone()
 print ("Database version : %s " % data)
-
 # Disconnect from MySQL server
 db.close()
